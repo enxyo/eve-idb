3d1b30000fbd931772c334a944b967f97fb6e390
enxyo <daniel.gruve@gmail.com>
2018-02-10 19:24:01 +0100

    updated .list command

--- idb_bot.js
+++ idb_bot.js
@@ -49,52 +49,9 @@ var connection = mysql.createConnection({
 });
 
 
-var jobList;
-
 /*******************************************
-**
+**  FUNCTIONS
 *******************************************/
-
-function getJobList(callback) {
-	var sql_query = 'SELECT * FROM eveJobs WHERE eveJobs.end_date > now() AND eveJobs.status = ? ORDER BY eveJobs.end_date';
-	var query = connection.query(sql_query, ['active'], function (error, results, fields)
-	{
-		if (error) return console.log('ERROR: An unexpected SQL error occured.');
-        var pending = results.length;
-        jobList = '**List of active jobs:**\n';
-
-        for(var i=0; i<results.length; i++) {
-            if((i != 0) && ((i%10) == 0)) {
-                //console.log(jobList);
-                client.channels.get('298828880452517889').send(jobList);
-                jobList = '**List of active jobs:**\n';
-            }
-            var currentDate = new Date();
-            var difference = results[i].end_date.getTime() - currentDate.getTime();
-            var daysDifference = Math.floor(difference/1000/60/60/24);
-            difference -= daysDifference*1000*60*60*24;
-            var hoursDifference = Math.floor(difference/1000/60/60);
-            difference -= hoursDifference*1000*60*60;
-            var minutesDifference = Math.floor(difference/1000/60);
-            difference -= minutesDifference*1000*60;
-            var secondsDifference = Math.floor(difference/1000);
-
-            jobList += '**' + results[i].runs + '**x **' + results[i].typeName + '** on *' + results[i].characterName + '* finishes in **' + daysDifference + 'd-' + hoursDifference + 'h:' + minutesDifference + 'm:' + secondsDifference + 's**\n';
-            if( 0 === --pending ) {
-                callback(results.length); //callback if all results are processed
-            }
-        }
-	});
-	//console.log(query.sql);
-}
-
-function printJobList(total){
-    if((total%10) != 0) {
-        client.channels.get('298828880452517889').send(jobList);
-    }
-    //console.log('done');
-
-}
 
 function getActiveJobs(corp) {
     var deferred = Q.defer();
@@ -147,6 +104,44 @@ function getNextJobs(corp) {
     return deferred.promise;
 }
 
+function getReadyJobsCount(corp) {
+    var deferred = Q.defer();
+
+    var sql_query = 'SELECT COUNT(eveJobs.job_id) AS jobCount, eveJobs.installer_id, eveJobs.characterName FROM eveJobs WHERE eveJobs.end_date < now() AND eveJobs.status=? AND eveJobs.corporationID=? GROUP BY eveJobs.installer_id ORDER BY COUNT(eveJobs.job_id) DESC';
+    var query = connection.query(sql_query, ['active',corp], function (error, results, fields)
+    {
+        if (error) {
+            deferred.reject(error);
+        }
+        else {
+            deferred.resolve(results);
+        }
+    });
+    //console.log(query.sql);
+    return deferred.promise;
+}
+
+function getActiveJobsList(corp) {
+    var deferred = Q.defer();
+
+    var sql_query = 'SELECT * FROM eveJobs WHERE eveJobs.end_date > now() AND eveJobs.status = ? AND eveJobs.corporationID=? ORDER BY eveJobs.end_date';
+    var query = connection.query(sql_query, ['active',corp], function (error, results, fields)
+    {
+        if (error) {
+            deferred.reject(error);
+        }
+        else {
+            deferred.resolve(results);
+        }
+    });
+    //console.log(query.sql);
+    return deferred.promise;
+}
+
+
+/*******************************************
+** DISCORD
+*******************************************/
 
 client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
@@ -181,9 +176,45 @@ client.on("message", (message) => {
 	}
 
     if (command  === 'list') {
-		getJobList(printJobList);
-	}
+		Q.all([getActiveJobs('98068725'),getReadyJobs('98068725'),getNextJobs('98068725'),getReadyJobsCount('98068725'),getActiveJobsList('98068725')]).spread(function(active,ready,next,count,list) {
+            var msg = '**Job list for <insert corporation>**\nThere are **' + active.length + '** active jobs and **' + ready.length + '** jobs ready.';
+            if(count.length != 0) {
+                msg += ' *(';
+                for (var i = 0;i < count.length; i++) {
+                    msg += ' ' + count[0].jobCount + ' - ' + count[0].characterName + ' ';
+                }
+                msg +=')*\n\n';
+            }
+            message.reply(msg);
 
+            var jobList = '**List of active jobs:**\n';
+
+            for(var i=0; i<list.length; i++) {
+                if((i != 0) && ((i%10) == 0)) {
+                    //console.log(jobList);
+                    message.reply(jobList);
+                    jobList = '**List of active jobs:**\n';
+                }
+                var currentDate = new Date();
+                var difference = list[i].end_date.getTime() - currentDate.getTime();
+                var daysDifference = Math.floor(difference/1000/60/60/24);
+                difference -= daysDifference*1000*60*60*24;
+                var hoursDifference = Math.floor(difference/1000/60/60);
+                difference -= hoursDifference*1000*60*60;
+                var minutesDifference = Math.floor(difference/1000/60);
+                difference -= minutesDifference*1000*60;
+                var secondsDifference = Math.floor(difference/1000);
+
+                jobList += '**' + list[i].runs + '**x **' + list[i].typeName + '** on *' + list[i].characterName + '* finishes in **' + daysDifference + 'd-' + hoursDifference + 'h:' + minutesDifference + 'm:' + secondsDifference + 's**\n';
+
+            }
+
+            if((list.length%10) != 0) {
+                message.reply(jobList);
+            }
+
+        }).done();
+	}
 
     if (command  === 'info') {
         Q.all([getActiveJobs('98068725'),getReadyJobs('98068725'),getNextJobs('98068725')]).spread(function(active,ready,next) {
